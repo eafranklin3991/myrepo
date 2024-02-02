a1ca63360c7e668a43da4618582376e8d1b30b2a
eafranklin3991 <emifrank@uga.edu>
2024-02-02 15:08:24 -0500

    Finishing up hello world

---
+++ hello world.R
@@ -0,0 +1,10 @@
+#'---
+#'title: "hello world"
+#'author: "Emily Franklin"
+#'date: "`r Sys.Date()`"
+#'output: github_document
+#'---
+
+#+ r setup, include = FALSE
+## insert your brilliant WORKING code here
+print("hello world")
--- hello-world.md
+++ hello-world.md
@@ -3,9 +3,3 @@ hello world
 Emily Franklin
 2024-02-02
 
-``` r
-## insert your brilliant WORKING code here
-print("hello world")
-```
-
-    ## [1] "hello world"
